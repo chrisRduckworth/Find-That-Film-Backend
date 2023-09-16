f2a309aa676625747efb75551f1eba64a1b754fe
chrisRduckworth <c_duckworth@hotmail.com>
2023-09-16 21:55:35 +0100

    add tests for GET /api/actors

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -31,3 +31,37 @@ describe("GET /api/genres", () => {
       });
   });
 });
+
+describe("GET /api/actors?query=actor", () => {
+  it("should respond with a list of the first 10 actors that match the search query", () => {
+    return request(app)
+      .get("/api/actors?name=clooney")
+      .expect(200)
+      .then(({ body: { actors } }) => {
+        expect(actors).toHaveLength(10);
+        actors.forEach((actor) => {
+          expect(actor).toHaveProperty("name");
+          expect(actor).toHaveProperty("img");
+          expect(actor.knownFor).toHaveProperty("title");
+          expect(actor.knownFor).toHaveProperty("year");
+        });
+      });
+  });
+  it("should respond subsequent pages if passed page query", () => {
+    return request(app)
+      .get("/api/actors?name=john%20smith&page=6")
+      .expect(200)
+      .then(({ body: { actors } }) => {
+        expect(actors).toHaveLength(19);
+      });
+  });
+  it("should respond with 400 invalid page if sent invalid type for page", () => {
+    return request(app)
+      .get("/api/actors?name=clooney&page=bananas")
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Invalid Page");
+      });
+    Page;
+  });
+});
