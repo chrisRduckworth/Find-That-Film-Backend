3a93c3f87dd12e367a26b7108b354036a538e18c
chrisRduckworth <c_duckworth@hotmail.com>
2023-09-16 20:40:37 +0100

    add test for /api/genres

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,4 +1,33 @@
-const request = require("supertest")
-const app = require("../app")
-const axios = require("axios/dist/node/axios.cjs")
+const request = require("supertest");
+const app = require("../app");
 
+describe("GET /api/genres", () => {
+  it("should respond with a list of all genres", () => {
+    return request(app)
+      .get("/api/genres")
+      .expect(200)
+      .then(({ body: { genres } }) => {
+        expect(genres).toEqual([
+          "Action",
+          "Adventure",
+          "Animation",
+          "Comedy",
+          "Crime",
+          "Documentary",
+          "Drama",
+          "Family",
+          "Fantasy",
+          "History",
+          "Horror",
+          "Music",
+          "Mystery",
+          "Romance",
+          "Science Fiction",
+          "TV Movie",
+          "Thriller",
+          "War",
+          "Western",
+        ]);
+      });
+  });
+});
