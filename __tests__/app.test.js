ea2d236f8baa43457b4b4c593e82c63537941d2e
chrisRduckworth <c_duckworth@hotmail.com>
2023-09-18 17:55:15 +0100

    add GET /api/films tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,8 +9,8 @@ describe("GET /api/genres", () => {
       .then(({ body: { genres } }) => {
         genres.forEach((genre) => {
           expect(genre).toHaveProperty("id"),
-          expect(genre).toHaveProperty("name")
-        })
+            expect(genre).toHaveProperty("name");
+        });
       });
   });
 });
@@ -23,7 +23,7 @@ describe("GET /api/people?name=name", () => {
       .then(({ body: { people } }) => {
         expect(people).toHaveLength(10);
         people.forEach((person) => {
-          expect(person).toHaveProperty("id", expect.any(Number))
+          expect(person).toHaveProperty("id", expect.any(Number));
           expect(person).toHaveProperty("name");
           expect(person).toHaveProperty("img");
           expect(person.knownFor).toHaveProperty("title");
@@ -47,5 +47,96 @@ describe("GET /api/people?name=name", () => {
         expect(msg).toBe("Invalid Page");
       });
     Page;
+  });
+});
+
+describe("GET /api/films", () => {
+  it("should return a list of films with multiple actors", () => {
+    return request(app)
+      .get("/api/films?actors=1461%2C1892")
+      .expect(200)
+      .then(({ body: { films } }) => {
+        expect(films).toHaveLength(9);
+        films.forEach((film) => {
+          expect(film).toHaveProperty("id");
+          expect(film).toHaveProperty("title");
+          expect(film).toHaveProperty("poster");
+          expect(film).toHaveProperty("year");
+        });
+      });
+  });
+  it("should return a list of films when given a specific year", () => {
+    return request(app)
+      .get("/api/films?actors=1461&year=2002")
+      .expect(200)
+      .then(({ body: { films } }) => {
+        expect(films).toHaveLength(3);
+        films.forEach((film) => {
+          expect(film).toHaveProperty("id");
+          expect(film).toHaveProperty("title");
+          expect(film).toHaveProperty("poster");
+          expect(film).toHaveProperty("year", "2002");
+        });
+      });
+  });
+  it("should return a list of films when given a specific director", () => {
+    return request(app)
+      .get("/api/films?actors=1461&directors=11218")
+      .expect(200)
+      .then(({ body: { films } }) => {
+        expect(films).toHaveLength(1);
+        films.forEach((film) => {
+          expect(film).toHaveProperty("id");
+          expect(film).toHaveProperty("title");
+          expect(film).toHaveProperty("poster");
+          expect(film).toHaveProperty("year");
+        });
+      });
+  });
+  it("should return a list of films when given specific genres", () => {
+    return request(app)
+      .get("/api/films?actors=1461&genres=12%2C10751")
+      .expect(200)
+      .then(({ body: { films } }) => {
+        expect(films).toHaveLength(4);
+        films.forEach((film) => {
+          expect(film).toHaveProperty("id");
+          expect(film).toHaveProperty("title");
+          expect(film).toHaveProperty("poster");
+          expect(film).toHaveProperty("year");
+        });
+      });
+  });
+  it("should respond with 400 bad request if sent invalid year", () => {
+    return request(app)
+      .get("/api/films?actors=1461&year=banana")
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Invalid Year");
+      });
+  });
+  it("should respond with 400 bad request if sent invalid actor id", () => {
+    return request(app)
+      .get("/api/films?actors=1461%2Cbananas")
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Invalid Id");
+      });
+  });
+  it("should respond with 400 bad request if sent invalid director id", () => {
+    return request(app)
+      .get("/api/films?actors=1461&directors=bananas")
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Invalid Id");
+      });
+  });
+  it("should respond with 400 bad request if sent invalid genre id", () => {
+    return request(app)
+      .get("/api/films?actors=1461&genres=bananas")
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Invalid Id");
+      });
   });
 });
