490ff1abe5dc5dee0435797c095ccc1bb8a68add
chrisRduckworth <c_duckworth@hotmail.com>
2023-09-18 19:09:39 +0100

    add port environment variable to listen

--- listen.js
+++ listen.js
@@ -1,5 +1,7 @@
-const app = require("./app")
+const app = require("./app");
 
-app.listen(9090, () => {
-  console.log("listening on port 9090")
-})
+const port = process.env.PORT || 9090;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
